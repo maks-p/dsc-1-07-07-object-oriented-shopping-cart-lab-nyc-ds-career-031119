5b90adc77ff784d50502add4462be2e999ab50c7
Maks Pazuniak <mpazuniak@gmail.com>
2019-03-15 13:54:02 -0400

    Added Instance & Total

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,9 +1,13 @@
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+    def __init__(self, total=0, emp_discount=None, items=[]):
+      self.total = total
+      self.emp_discount = emp_discount
+      self.items = items
+
     def add_item(self, name, price, quantity=1):
        pass
+
     def mean_item_price(self):
        pass
 
@@ -15,3 +19,6 @@ class ShoppingCart:
 
     def void_last_item(self):
         pass
+
+shopping_cart = ShoppingCart()
+print(shopping_cart.total)
